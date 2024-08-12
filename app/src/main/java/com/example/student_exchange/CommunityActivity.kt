bb91d8cd8649450e12ec72e5c8fadaf2d3839c96
onlyoneprivate <pllm098@naver.com>
2024-08-12 23:28:25 +0900

    mypage, community

--- app/src/main/java/com/example/student_exchange/CommunityActivity.kt
+++ app/src/main/java/com/example/student_exchange/CommunityActivity.kt
@@ -1,149 +1,27 @@
 package com.example.student_exchange
 
-import HotSectionFragment
-import android.content.Intent
 import android.os.Bundle
-import android.view.MenuItem
-import android.view.View
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.student_exchange.adapter.HotAdapter
-import com.example.student_exchange.databinding.ActivityCommunityBinding
-import com.example.student_exchange.model.HotItem
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class CommunityActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityCommunityBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityCommunityBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_community)
 
-        // Set up Toolbar
-        binding.toolbarIcon0.setColorFilter(ContextCompat.getColor(this, R.color.titleblack))
-
-        // Set up Fragments
         if (savedInstanceState == null) {
+            // Hot Section Fragment 추가
+            val hotFragmentContainer = findViewById<FrameLayout>(R.id.fragmentHotSectionContainer)
             supportFragmentManager.beginTransaction()
-                .replace(R.id.fragmentHotSectionContainer, HotSectionFragment())
+                .replace(hotFragmentContainer.id, HotSectionFragment())
                 .commit()
 
+            // Post Section Fragment 추가
+            val postFragmentContainer = findViewById<FrameLayout>(R.id.fragmentPostSectionContainer)
             supportFragmentManager.beginTransaction()
-                .replace(R.id.fragmentPostSectionContainer, PostSectionFragment())
+                .replace(postFragmentContainer.id, PostSectionFragment())
                 .commit()
         }
-
-        // Toolbar Icon Click
-        binding.toolbarIcon0.setOnClickListener {
-            showWritePostFragment()
-        }
-
-        // Bottom Navigation setup
-        binding.mainBnv.setOnItemSelectedListener { item: MenuItem ->
-            when (item.itemId) {
-                R.id.travelFragment -> {
-                    replaceFragment(TravelFragment())
-                    true
-                }
-                R.id.communityActivity -> {
-                    startActivity(Intent(this, CommunityActivity::class.java))
-                    true
-                }
-                R.id.recordFragment -> {
-                    replaceFragment(RecordFragment())
-                    true
-                }
-                R.id.mypageActivity -> {
-                    startActivity(Intent(this, MyPageActivity::class.java))
-                    true
-                }
-                else -> false
-            }
-        }
-
-        setupRecyclerView() // Ensure RecyclerView setup is done
-    }
-
-    private fun showWritePostFragment() {
-        // Hide other parts of the UI
-        hideMainLayout()
-
-        val fragment = WritePostFragment()
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragmentPostSectionContainer, fragment)
-            .addToBackStack(null)
-            .commit()
-    }
-
-    private fun replaceFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragmentPostSectionContainer, fragment)
-            .addToBackStack(null)
-            .commit()
-    }
-
-    private fun hideMainLayout() {
-        binding.fragmentHotSectionContainer.visibility = View.GONE
-        binding.fragmentPostSectionContainer.visibility = View.GONE
-        binding.mainBnv.visibility = View.GONE
-        binding.toolbar.visibility = View.GONE
-        // Assuming 'fragmentContainer' should be replaced with 'fragmentPostSectionContainer'
-        binding.fragmentPostSectionContainer.visibility = View.VISIBLE
-    }
-
-    private fun showMainLayout() {
-        binding.fragmentHotSectionContainer.visibility = View.VISIBLE
-        binding.fragmentPostSectionContainer.visibility = View.VISIBLE
-        binding.mainBnv.visibility = View.VISIBLE
-        binding.toolbar.visibility = View.VISIBLE
-        // Assuming 'fragmentContainer' should be replaced with 'fragmentPostSectionContainer'
-        binding.fragmentPostSectionContainer.visibility = View.GONE
-    }
-
-    override fun onBackPressed() {
-        if (supportFragmentManager.backStackEntryCount > 0) {
-            super.onBackPressed()
-            showMainLayout()  // Return to main layout when back is pressed
-        } else {
-            super.onBackPressed()
-        }
-    }
-
-    // ViewPager2에 어댑터 설정
-//        val adapter = MyPagerAdapter(this)
-//        viewPager.adapter = adapter
-
-//        // TabLayout과 ViewPager2 연결
-//        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-//            when (position) {
-//                0 -> tab.text = "전체"
-//                1 -> tab.text = "UCLA"
-//                2 -> tab.text = "서울여대"
-//                3 -> tab.text = "꿀팁"
-//                4 -> tab.text = "후기"
-//            }
-//        }.attach()
-
-
-    private fun setupRecyclerView() {
-        val items = listOf(
-            HotItem("UCLA 근처 맛집 주천!!", 120, R.drawable.eye_viewer),
-            HotItem("UCLA 근처 맛집 주천!!", 120, R.drawable.eye_viewer),
-            HotItem("UCLA 근처 맛집 주천!!", 120, R.drawable.eye_viewer),
-            HotItem("UCLA 근처 맛집 주천!!", 120, R.drawable.eye_viewer),
-            HotItem("UCLA 근처 맛집 주천!!", 120, R.drawable.eye_viewer)
-        )
-
-        val hotAdapter = HotAdapter(items)
-//        binding.recyclerView.layoutManager = LinearLayoutManager(this)
-//        binding.recyclerView.adapter = hotAdapter
-    }
-
-    fun hideBottomNavigation() {
-        binding.mainBnv.visibility = View.GONE
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -16,3 +16,16 @@ plugins {
 //        // in the individual module build.gradle files
 //    }
 //}
+//buildscript {
+//    ext.kotlin_version = "1.5.21"
+//    repositories {
+//        google()
+//        mavenCentral()
+//    }
+//    dependencies {
+//        classpath "com.android.tools.build:gradle:4.2.2"
+//        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+//        // NOTE: Do not place your application dependencies here; they belong
+//        // in the individual module build.gradle files
+//    }
+//}
